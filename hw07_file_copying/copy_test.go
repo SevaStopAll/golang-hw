93e013a519a1a26afca9001d6d8a16f77b7faccb
vsevolod <ososkov_va@nlmk>
2025-08-18 19:11:43 +0300

    change way of creating files' paths

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -2,50 +2,64 @@ package main
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestCopy(t *testing.T) {
+
 	t.Run("limit 0 offset 0", func(t *testing.T) {
-		err := Copy("testdata\\input.txt",
-			"testdata\\test2", 0, 0)
+		pathFrom := filepath.Join("testData", "input.txt")
+		pathTo := filepath.Join("testdata", "test.txt")
+		err := Copy(pathFrom,
+			pathTo, 0, 0)
 		require.NoError(t, err)
-		defer os.Remove("testdata\\test2")
+		defer os.Remove(pathTo)
 	})
 
 	t.Run("offset 0 limit 10", func(t *testing.T) {
-		err := Copy("testdata\\input.txt",
-			"testdata\\test2", 0, 10)
+		pathFrom := filepath.Join("testData", "input.txt")
+		pathTo := filepath.Join("testdata", "test.txt")
+		err := Copy(pathFrom,
+			pathTo, 0, 10)
 		require.NoError(t, err)
 		defer os.Remove("testdata\\test2")
 	})
 
 	t.Run("offset 0 limit 1000", func(t *testing.T) {
-		err := Copy("testdata\\input.txt",
-			"testdata\\test2", 0, 1000)
+		pathFrom := filepath.Join("testData", "input.txt")
+		pathTo := filepath.Join("testdata", "test.txt")
+		err := Copy(pathFrom,
+			pathTo, 0, 1000)
 		require.NoError(t, err)
 		defer os.Remove("testdata\\test2")
 	})
 
 	t.Run("offset 100 limit 1000", func(t *testing.T) {
-		err := Copy("testdata\\input.txt",
-			"testdata\\test2", 100, 1000)
+		pathFrom := filepath.Join("testData", "input.txt")
+		pathTo := filepath.Join("testdata", "test.txt")
+		err := Copy(pathFrom,
+			pathTo, 100, 1000)
 		require.NoError(t, err)
 		defer os.Remove("testdata\\test2")
 	})
 
 	t.Run("offset 100 limit 1000", func(t *testing.T) {
-		err := Copy("testdata\\input.txt",
-			"testdata\\test2", 100, 10000)
+		pathFrom := filepath.Join("testData", "input.txt")
+		pathTo := filepath.Join("testdata", "test.txt")
+		err := Copy(pathFrom,
+			pathTo, 100, 10000)
 		require.NoError(t, err)
 		defer os.Remove("testdata\\test2")
 	})
 
 	t.Run("offset 6000 limit 1000", func(t *testing.T) {
-		err := Copy("testdata\\input.txt",
-			"testdata\\test2", 6000, 1000)
+		pathFrom := filepath.Join("testData", "input.txt")
+		pathTo := filepath.Join("testdata", "test.txt")
+		err := Copy(pathFrom,
+			pathTo, 6000, 1000)
 		require.NoError(t, err)
 		defer os.Remove("testdata\\test2")
 	})
