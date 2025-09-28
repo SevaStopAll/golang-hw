cb121eb58d6f0a66af827d02f0aceb349bbf40bb
vsevolod <ososkov_va@nlmk>
2025-09-28 14:18:58 +0300

    changed main.go

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"os"
 )
 
 func main() {
-	cmdWithArgs := os.Args[2:]
-	dir := os.Args[1]
-	envDirMap, err := ReadDir(dir)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	exitCode := RunCmd(cmdWithArgs, envDirMap)
-	os.Exit(exitCode)
+	env, _ := ReadDir(os.Args[1])
+	code := RunCmd(os.Args[2:], env)
+	os.Exit(code)
 }
